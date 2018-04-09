88e04f0722b34f9f20773c6da7f6b3973f80988a
madhur gupta <madhur.gupta@infinitylabs.in>
2018-04-09 16:39:54 +0530

    html

--- mopa1/settings.py
+++ mopa1/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
     }
